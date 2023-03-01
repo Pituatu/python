666ed22c84a5ef17508cc3e867bb335d4f7ed747
Lucas Tavares <engenheiro.lucastavares@gmail.com>
2023-03-01 09:02:23 -0300

    exercicio 15

---
+++ DesafiosCurso/Ex15.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia um número real e
+# mostre sua parte inteira.
+import math
+numero = float(input('Digite o número: '))
+print(f'a parte inteira do número é {math.trunc(numero)}')
